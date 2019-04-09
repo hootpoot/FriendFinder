b94e198dd0d18f8129bd8a6ca9c1720f708b3b17
Katie <47231728+hootpoot@users.noreply.github.com>
2019-04-09 12:51:50 -0500

    completed html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,14 @@
-var frogData = require("../data/friends");
+var frogData = require("../data/frogs.js");
 
-module.exports = function(app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
-  
-    app.get("/api/frogs", function(req, res) {
-      res.json(friends);
-    });
-  
+module.exports = function (app) {
     
-    app.post("/api/clear", function(req, res) {
-      // Empty out the arrays of data
-      friends.length = 0;
-      
-  
-      res.json({ ok: true });
+    app.get("/api/frogs", function (req, res) {
+        res.json(frogData);
     });
-  };
+
+
+    app.post("/api/frogs", function (req, res) {
+        frogData.push(req.body);
+        res.json(true);
+    });
+};
